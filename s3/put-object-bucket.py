18abc3b6b7379702d3d0fca5dc4856e909009be7
Larrygf02 <raulgf_02@hotmail.com>
2019-06-07 12:22:32 -0500

    get list the object in a bucket

--- s3/put-object-bucket.py
+++ s3/put-object-bucket.py
@@ -18,6 +18,14 @@ def upload():
     s3.Bucket('python-boto301').put_object(Key=name_file, Body=file)
     return '<h1>File saved to S3</h1>'
     
+@app.route('/get-objects', methods=['GET'])
+def get_objects():
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket('python-boto301')
+    list = bucket.objects.all()
+    for object in list:
+        print(object)
+    return '<h1>Cargando los objetos</h1>'
 
 if __name__ == '__main__':
     app.run(debug=True)
