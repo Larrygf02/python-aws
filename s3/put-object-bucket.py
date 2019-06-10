081478b406ba664b8fba8fa64e4052f96de08861
Larrygf02 <raulgf_02@hotmail.com>
2019-06-10 12:23:40 -0500

    Download files in boto3

--- s3/put-object-bucket.py
+++ s3/put-object-bucket.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 import boto3
 
 app = Flask(__name__)
@@ -23,9 +23,25 @@ def get_objects():
     s3 = boto3.resource('s3')
     bucket = s3.Bucket('python-boto301')
     list = bucket.objects.all()
+    list_name = []
     for object in list:
-        print(object)
-    return '<h1>Cargando los objetos</h1>'
+        list_name.append(object)
 
+    return render_template('objects.html', objects=list_name)
+
+@app.route('/download/<object>', methods=['GET'])
+def download(object):
+    import botocore
+    s3 = boto3.resource('s3')
+    try:
+        s3.Bucket('python-boto301').download_file(object, object)
+        return '<h1>Se descargo con exito</h1>'
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == '404':
+            return '<h1>El archivo no existe</h1>'
+        else:
+            return '<h1>Opps<h1>'
+    
+        
 if __name__ == '__main__':
     app.run(debug=True)
